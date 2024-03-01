5de1fb2f934d09558fd1e66436073980208e1c6e
Liquid098 <144435181+Liquid098@users.noreply.github.com>
2024-03-01 19:07:29 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
-int main() {
-	double a, b, c, v, s;
-	cout << "Vvedite storonu A: ";
-	cin >> a;
-	cout << "Vvedite storonu B: ";
-	cin >> b;
-	cout << "Vvedite storonu C: ";
-	cin >> c;
-	v = a * b * c;
-	s = 2 * (a * b + b * c + a * c);
-	cout << "Ob'yom pryamougol'nogo parallelepipeda: " << v << endl;
-	cout << "Ploshyad' bokovoy poverkhnosti: " << s;
-	return 0;
+int main()
+{
+	cout << "\n Marrix18. \n";
+	int m, n, k;
+	int amount = 0;
+	long long int composition = 1;
+	cout << "enter the dimension M and N or the array: ";
+	cin >> m >> n;
+	int array[100][100] = {};
+	cout << "enter array elements:" << endl;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> array[i][j];
+		}
+	}
+	cout << "enter value K:";
+	cin >> k;
+	for (int i = 0; i < m; i++)
+	{
+		int j = k - 1;
+		amount += array[i][j];
+		composition *= array[i][j];
+	}
+	cout << "Sum:" << amount << "Work:" << composition;
 }
